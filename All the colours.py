55bc7040223309cea60919f5c7e467ceaf047c80
SpyrosRoum <spyrosr1@gmail.com>
2018-10-20 21:26:26 +0300

    Add files via upload

--- All the colours.py
+++ All the colours.py
@@ -1,24 +1,16 @@
 import pyautogui as pag
 import win32api
 import webcolors
+import waiting
 import tkinter as tk
 
-# Get state of mouse
-state_left = win32api.GetKeyState(0x01)  # Left button down = 0 or 1. Button up = -127 or -128
 
-# Wait for mouse click
-while True:
-    a = win32api.GetKeyState(0x01)
+waiting.wait(lambda state_left=win32api.GetKeyState(0x01): state_left !=
+             win32api.GetKeyState(0x01), sleep_seconds=0.01)
 
-    if a != state_left:  # Button state changed
-        state_left = a
+x, y = win32api.GetCursorPos()
+rgb = pag.pixel(x, y)
 
-        # Get the x,y of cursor and rgb of pixel on that x,y
-        x, y = win32api.GetCursorPos()
-        rgb = pag.pixel(x, y)
-
-        break
-# Set Hex based on the rgb
 Hex = webcolors.rgb_to_hex(rgb)
 r, g, b = rgb
 
@@ -81,23 +73,24 @@ class ST_Win:
         self.master.title("Shades")
         # Tints
         for i in range(3):
-            btn = self.buttons[f'btn{i}'] = tk.Button(self.master, width=8, bg=tColours[i],
-                                                      command=lambda: self.on_s_click('Blue'))
-            btn.grid(row=1, column=i)
+            tBtn = self.buttons[f'btn{i}'] = tk.Button(self.master, width=8, bg=tColours[i])
+            tBtn.config(command=lambda btn=tBtn: self.on_s_click(btn))
+            tBtn.grid(row=1, column=i)
         # Shades
         for i in range(3):
-            btn = self.buttons[f'btn{i+3}'] = tk.Button(self.master, width=8, bg=sColours[i],
-                                                        command=lambda: self.on_s_click('Red'))
-            btn.grid(row=2, column=i)
+            sBtn = self.buttons[f'btn{i+3}'] = tk.Button(self.master, width=8, bg=sColours[i])
+            sBtn.config(command=lambda btn=sBtn: self.on_s_click(btn))
+            sBtn.grid(row=2, column=i)
+
+    def on_s_click(self, btn):
+        col = btn.cget('bg')
+        tk.Canvas(self.master, width=90, height=90, bg=col).grid(row=4, column=0)
 
         txt = tk.Text(self.master, width=60, height=1, bg='#f0f0f0')
-        txt.insert('1.0', f'Rgb value is: rgb. The Hex value is: {tColours[0]}')
+        txt.insert(
+            '1.0', f"Rgb value is: {webcolors.hex_to_rgb(col)[0:3]}. The Hex value is: {col}")
         txt['state'] = tk.DISABLED
         txt.grid(row=4, column=1)
-
-    def on_s_click(self, co):
-        tk.Canvas(self.master, width=90, height=90, bg=co).grid(row=4, column=0)
-        print(self.buttons)
 
 
 # Create the main window
@@ -114,3 +107,20 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+# Canvas(root, width=90, height=90, bg=Hex).grid(row=1, column=0)
+#
+# Button(root, text='Shades', width=14, command=Shades_win).grid(row=3, column=1)
+#
+#
+# text = Text(root, width=60, height=6, bg='#f0f0f0')
+# text.insert('1.0', 'Rgb value is: {}. The Hex value is: {}'.format(rgb, Hex))
+# text['state'] = DISABLED
+# text.grid(row=1, column=1)
+#
+#
+# root.mainloop()
+#
+# Label(root, text=(''), font='none 12 bold', bg=Hex). grid(row=1, column=0, sticky='w')
+#
+# Button(root, text="", width=14, bg=Hex) .grid(row=1, column=0, sticky=W)
